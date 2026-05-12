5422eb99c1c4b4c13acb02969993b8b30898b250
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-05-12 14:41:15 +0200

    Feat: Add schema config

---
+++ Complexion/Complexion.Migrations/MigrationRunner.cs
@@ -0,0 +1,25 @@
+﻿using DbUp;
+using System.Reflection;
+
+namespace Complexion.Migrations;
+
+public class MigrationRunner
+{
+    public static void Run(string connectionString)
+    {
+        EnsureDatabase.For.SqlDatabase(connectionString);
+
+        var upgrader = DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            throw new Exception("Migration failed", result.Error);
+        }
+    }
+}
---
+++ Complexion/Complexion.Migrations/Scripts/V1__CreateSchemas.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA skin;
+GO
+
+CREATE SCHEMA config;
+GO
