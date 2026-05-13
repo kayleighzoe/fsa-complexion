f4e0819986e4615cc5988b5cd020f6fcacd96155
Kayleigh <kayleigh.james@kcpivot.co.za>
2026-05-13 14:41:01 +0200

    Fix: Indentation

--- Complexion/Complexion.Migrations/Scripts/V5__CreateBridgeTable.sql
+++ Complexion/Complexion.Migrations/Scripts/V5__CreateBridgeTable.sql
@@ -3,15 +3,15 @@
 )
 BEGIN
 	CREATE TABLE ProductRecommendation (
-	RecommendationId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-	UserId UNIQUEIDENTIFIER NOT NULL,
-	ProductId UNIQUEIDENTIFIER NOT NULL,
-	SkinProfileId UNIQUEIDENTIFIER NOT NULL,
-	Comment VARCHAR(255),
-	CreatedAt DATETIME NOT NULL,
-	CONSTRAINT FK_ProductRecommendation_User FOREIGN KEY (UserId) REFERENCES Users(UserId),
-	CONSTRAINT FK_ProductRecommendation_Product FOREIGN KEY (ProductId) REFERENCES Product(ProductId),
-	CONSTRAINT FK_ProductRecommendation_SkinProfile FOREIGN KEY (SkinProfileId) REFERENCES SkinProfile(SkinProfileId)
+		RecommendationId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
+		UserId UNIQUEIDENTIFIER NOT NULL,
+		ProductId UNIQUEIDENTIFIER NOT NULL,
+		SkinProfileId UNIQUEIDENTIFIER NOT NULL,
+		Comment VARCHAR(255),
+		CreatedAt DATETIME NOT NULL,
+		CONSTRAINT FK_ProductRecommendation_User FOREIGN KEY (UserId) REFERENCES Users(UserId),
+		CONSTRAINT FK_ProductRecommendation_Product FOREIGN KEY (ProductId) REFERENCES Product(ProductId),
+		CONSTRAINT FK_ProductRecommendation_SkinProfile FOREIGN KEY (SkinProfileId) REFERENCES SkinProfile(SkinProfileId)
 	);
 END
 GO
