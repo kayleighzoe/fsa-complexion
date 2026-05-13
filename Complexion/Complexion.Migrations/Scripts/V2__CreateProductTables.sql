64b092e764f895245c972ac62791513f85f299ad
Kayleigh <kayleigh.james@kcpivot.co.za>
2026-05-13 14:18:14 +0200

    Feat: Add core tables

--- Complexion/Complexion.Migrations/Scripts/V2__CreateProductTables.sql
+++ Complexion/Complexion.Migrations/Scripts/V2__CreateProductTables.sql
@@ -1,17 +1,32 @@
-﻿CREATE TABLE product.Brand (
+﻿IF NOT EXISTS (
+    SELECT * FROM sys.tables WHERE name LIKE 'product.Brand' 
+)
+BEGIN
+    CREATE TABLE product.Brand (
     BrandId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     Name VARCHAR(25) NOT NULL
-);
+    );
+END
 GO
 
-CREATE TABLE product.Category (
+IF NOT EXISTS (
+    SELECT * FROM sys.tables WHERE name LIKE 'product.Category'
+)
+BEGIN
+    CREATE TABLE product.Category (
     CategoryId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     Name VARCHAR(25) NOT NULL
-);
+    );
+END
 GO
 
-CREATE TABLE product.PriceTier (
+IF NOT EXISTS (
+    SELECT * FROM sys.tables WHERE name LIKE 'product.PriceTier'
+)
+BEGIN
+    CREATE TABLE product.PriceTier (
     PriceTierId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     Name VARCHAR(15) NOT NULL
-);
+    );
+END
 GO
--- Complexion/Complexion.Migrations/Scripts/V3__CreateSkinTables.sql
+++ Complexion/Complexion.Migrations/Scripts/V3__CreateSkinTables.sql
@@ -1,11 +1,21 @@
-﻿CREATE TABLE skin.Shade (
+﻿IF NOT EXISTS (
+    SELECT * FROM sys.tables WHERE name LIKE 'skin.Shade'
+)
+BEGIN
+    CREATE TABLE skin.Shade (
     ShadeId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     Name VARCHAR(25) NOT NULL
-);
+    );
+END
 GO
 
-CREATE TABLE skin.Undertone (
+IF NOT EXISTS (
+    SELECT * FROM sys.tables WHERE name LIKE 'skin.Undertone'
+)
+BEGIN
+    CREATE TABLE skin.Undertone (
     UndertoneId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
     Name VARCHAR(25) NOT NULL
-);
+    );
+END
 GO
