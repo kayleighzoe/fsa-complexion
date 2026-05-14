c02cac36c3c25889a4a48addb7843aae29d48e6b
Kayleigh <kayleigh.james@kcpivot.co.za>
2026-05-14 10:44:58 +0200

    Fix: Remove brand lookup table

--- Complexion/Complexion.Migrations/Scripts/V2__CreateProductTables.sql
+++ Complexion/Complexion.Migrations/Scripts/V2__CreateProductTables.sql
@@ -1,15 +1,4 @@
 ﻿IF NOT EXISTS (
-    SELECT * FROM sys.tables WHERE name LIKE 'product.Brand' 
-)
-BEGIN
-    CREATE TABLE product.Brand (
-        BrandId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-        Name VARCHAR(25) NOT NULL
-    );
-END
-GO
-
-IF NOT EXISTS (
     SELECT * FROM sys.tables WHERE name LIKE 'product.Category'
 )
 BEGIN
