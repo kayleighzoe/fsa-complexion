697ce34f8d0a59e8f74ba9325496872bd52bfd91
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-05-20 11:08:02 +0200

    Feat: Add catelogue tables

---
+++ Complexion/Complexion.Migrations/Scripts/20260520104701CreatePriceTierTable.sql
@@ -0,0 +1,16 @@
+﻿IF NOT EXISTS
+(
+    SELECT
+        1
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_SCHEMA = 'catalogue'
+        AND TABLE_NAME = 'PriceTier'
+)
+BEGIN
+    CREATE TABLE catalogue.PriceTier
+    (
+        PriceTierId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+        Name VARCHAR(15) NOT NULL
+    );
+END
+GO
