4b9dc484530a9d983c881fd53ee60f08b649e7ae
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-06-19 10:52:53 +0200

    feat: Add data to underone table

--- Complexion/Complexion.Migrations/Scripts/20260618120000InsertLookupTableData.sql
+++ Complexion/Complexion.Migrations/Scripts/20260618120000InsertLookupTableData.sql
@@ -22,3 +22,25 @@ BEGIN
 	);
 END
 GO
+
+IF NOT EXISTS (SELECT 1 FROM skin.Undertone)
+BEGIN
+    INSERT INTO skin.Undertone (
+		Name
+	) VALUES(
+		'Cool'
+	),
+    (
+		'Cool-Neutral'
+	),
+    (
+		'True Neutral'
+	),
+    (
+		'Warm-Neutral'
+	),
+    (
+		'Warm'
+	);
+END
+GO
