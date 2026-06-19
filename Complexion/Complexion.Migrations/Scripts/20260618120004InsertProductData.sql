c448587090607fcedb26d5e68769486b38e121ef
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-06-19 13:56:38 +0200

    Feat: Add product data

---
+++ Complexion/Complexion.Migrations/Scripts/20260618120004InsertProductData.sql
@@ -0,0 +1,27 @@
+IF NOT EXISTS (SELECT 1 FROM dbo.Product)
+BEGIN
+	INSERT INTO dbo.Product (
+		ProductId,
+		CategoryId,
+		PriceTierId,
+		Name,
+		Brand,
+		ShadeName
+	) VALUES (
+		NEWID(),
+		1,
+		1,
+		'Studio Fix Fluid',
+		'MAC',
+		'nc44.5'
+	),
+	(
+		NEWID(),
+		4,
+		2,
+		'Fitme Spot Concealer',
+		'Maybelline',
+		'75'
+	)
+END
+GO
