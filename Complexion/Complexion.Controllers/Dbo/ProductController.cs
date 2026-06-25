2b3a8f7631f6cd1498c3b5a98e7ec5149cc5e2a3
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-06-25 08:18:06 +0200

    Fix: Add coach feedback

--- Complexion/Complexion.Controllers/Dbo/ProductController.cs
+++ Complexion/Complexion.Controllers/Dbo/ProductController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Complexion.Controllers.Dbo
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class ProductController : ControllerBase
     {
         private readonly IProductService _service;
