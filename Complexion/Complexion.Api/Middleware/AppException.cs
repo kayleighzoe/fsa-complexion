a7b0d6ca6305d4bef1e3fe1dbb39bd76b64b1af8
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-06-25 10:33:44 +0200

    Feat: Add app exception class

---
+++ Complexion/Complexion.Api/Middleware/AppException.cs
@@ -0,0 +1,13 @@
+﻿namespace Complexion.Api.Middleware
+{
+    public class AppException : Exception
+    {
+        public int StatusCode { get; }
+
+        public AppException(string message, int statusCode = 500)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
