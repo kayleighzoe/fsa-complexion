804e79d95b053008c58f5485be5a259a476843f0
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-08-12 12:01:36 +0200

    test(api): add unit test for get all categories service

---
+++ Complexion/Complexion.Tests/Services/Catalogue/CatalogueCategoryServiceTests.cs
@@ -0,0 +1,42 @@
+using Complexion.Models.Catalogue;
+using Complexion.Repository.Catalogue;
+using Complexion.Services.Catalogue;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Complexion.Tests.Services.Catalogue
+{
+    [TestFixture]
+    public class CatalogueCategoryServiceTests
+    {
+        private ICatalogueCategoryRepository _repository;
+        private CatalogueCategoryService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repository = Substitute.For<ICatalogueCategoryRepository>();
+            _service = new CatalogueCategoryService(_repository);
+        }
+
+        [Test]
+        public async Task GetAllCategoriesAsync_ReturnsCategoriesFromRepository()
+        {
+            // Arrange
+            var expectedCategories = new List<CatalogueCategory>
+            {
+                new CatalogueCategory { CategoryId = 1, Name = "High-coverage Foundation" },
+                new CatalogueCategory { CategoryId = 2, Name = "Concealer" }
+            };
+
+            _repository.GetAllCategoriesAsync().Returns(expectedCategories);
+
+            // Act
+            var result = await _service.GetAllCategoriesAsync();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedCategories));
+            await _repository.Received(1).GetAllCategoriesAsync();
+        }
+    }
+}
