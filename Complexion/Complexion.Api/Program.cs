46f528346758e7cb59ec13c45271d6a9ad4fb74e
Kayleigh James <kayleigh.james@kcpivot.co.za>
2026-08-12 11:29:21 +0200

    fix(api): remove redundancy

--- Complexion/Complexion.Api/Program.cs
+++ Complexion/Complexion.Api/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, services, configuration) =>
 {
-    configuration.ReadFrom.Configuration(context.Configuration).Enrich.FromLogContext();
+    configuration.ReadFrom.Configuration(context.Configuration);
 });
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
